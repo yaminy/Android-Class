a52975fa5ee59f57caf6eff460334f673118866d
Yamin Yazdanpanah <yaminyazdanpanah@gmail.com>
2018-08-13 18:56:42 +0430

    session 2 calculator add

--- app/src/main/java/com/yamin/session1/MainActivity.java
+++ app/src/main/java/com/yamin/session1/MainActivity.java
@@ -2,29 +2,50 @@ package com.yamin.session1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-
-    String buttonTxt = "Button Text";
+    EditText editTextNumber1, editTextNumber2;
+    Button buttonAdd;
+    TextView textViewSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.btn1);
+        inItView();
 
-        button.setText(buttonTxt);
+        handleOperation();
 
-//        setColor(bbbb);
 
     }
 
-//    void setColor(Button bndfd) {
-////        bndfd.setBackgroundColor(R.color.colorAccent);
-//    }
+    private void handleOperation() {
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String edit1 = editTextNumber1.getText().toString();
+                String edit2 = editTextNumber2.getText().toString();
+
+                textViewSum.setText(Integer.valueOf(edit1) + Integer.valueOf(edit2));
+
+            }
+        });
+
+    }
+
+    private void inItView() {
+        editTextNumber1 = (EditText) findViewById(R.id.editTxt_number_1);
+        editTextNumber2 = (EditText) findViewById(R.id.editTxt_number_2);
+        buttonAdd = (Button) findViewById(R.id.button_add);
+        textViewSum = (TextView) findViewById(R.id.txtview_sum);
+    }
+
 
 }
